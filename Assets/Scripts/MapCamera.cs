1c4b7cdd30beb47623ba081317c359d376718c40
00Jul1an00 <mrjulik.beletsky@yandex.ru>
2023-02-20 12:31:25 +0300

    /

---
+++ Assets/Scripts/MapCamera.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCamera : MonoBehaviour
+{
+    private Camera _camera;
+    private Rect _cameraRect;
+
+    private void Start()
+    {
+
+    }
+
+
+}
