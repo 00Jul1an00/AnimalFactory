f08da0f844e382ce1301d8f075fe5cba462a390b
Bonifa32 <Salmon3221@mail.ru>
2023-02-22 17:43:12 +0300

    SpeedUpManager 2

--- Assets/Scripts/SpeedUpManager.cs
+++ Assets/Scripts/SpeedUpManager.cs
@@ -4,23 +4,42 @@ using UnityEngine;
 
 public class SpeedUpManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _trigger;
+    [SerializeField] private GameObject[] _triggers = new GameObject[3];
+    [SerializeField] private IterationTrigger[] _itTriggers = new IterationTrigger[3];
 
-    private IterationTrigger _itTrigger;
+    [SerializeField] private int _speedMultiplier;
+    [SerializeField] private float _speedUpDurationInSec = 10;
 
     private void Start()
     {
-        _itTrigger = _trigger.GetComponent<IterationTrigger>();
+        _speedUpDurationInSec *= 1000f;
+        _triggers = GameObject.FindGameObjectsWithTag("IterationTrigger");
+        for (int i = 0; i < _triggers.Length; i++) 
+        {
+            _itTriggers[i] = _triggers[i].GetComponent<IterationTrigger>();
+        }
+        
     }
 
-    //public void SpeedUp()
-    //{
-    //    _itTrigger.ProductionDelay /= 2;
-     //   print(_itTrigger.ProductionDelay);
-    //}
+    private void Update()
+    {
+        
+    }
+
+    public void SpeedUp()
+    {
+        for (int i = 0; i < _itTriggers.Length; i++)
+        {
+            _itTriggers[i].ProductionDelay /= _speedMultiplier;
+        }
+
+        
+    }
 
     //private void OnMouseDown()
     //{
-    //    SpeedUp();
+     //   SpeedUp();
     //}
+
+    
 }
