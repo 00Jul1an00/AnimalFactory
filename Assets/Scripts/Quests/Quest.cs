7e379318e4d7ab1447d9a73f1915bcbf0679cbb2
00Jul1an00 <mrjulik.beletsky@yandex.ru>
2023-03-31 23:13:57 +0300

    Update Quest.cs

--- Assets/Scripts/Quests/Quest.cs
+++ Assets/Scripts/Quests/Quest.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 
+public enum QuestType
+{
+    ClickCount,
+    UpgradeLvl,
+
+}
+
 public enum QuestReward
 {
     Money,
@@ -13,17 +20,20 @@ public enum QuestReward
 public class Quest : MonoBehaviour
 {
     [SerializeField] private QuestReward _rewardType;
+    [SerializeField] private QuestType _questType;
     //если передавать в rewardType кейсы или предметы, нужно передовать в rewardValue id предмета
     [SerializeField] private int _rewardValue;
-
+    [SerializeField] private string _description;
     public QuestReward RewardType { get { return _rewardType; } protected set { _rewardType = value; } }
     public bool IsComplete { get; protected set; }
     public int QuestId { get; protected set; }
 
     public event Action QuestComplete;
+    public delegate Func<bool> QuestPredicate();
 
     private void Init() 
     {
+
     }
 
     public virtual void Complete()
@@ -36,8 +46,7 @@ public class Quest : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            print(_rewardType);
-            print(_rewardValue);
+            
         }
     }
 }
