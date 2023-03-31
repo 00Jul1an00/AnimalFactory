c4cd799e7f23d8d1a5e33b7d78965ecd5f6087c0
Bonifa32 <Salmon3221@mail.ru>
2023-03-31 19:33:49 +0300

    32

--- Assets/Scripts/SpeedUpManager.cs
+++ Assets/Scripts/SpeedUpManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpeedUpManager : MonoBehaviour
@@ -8,13 +9,12 @@ public class SpeedUpManager : MonoBehaviour
     [SerializeField] private IterationTrigger[] _itTriggers = new IterationTrigger[3];
 
     [SerializeField] private int _speedMultiplier;
-    [SerializeField] private float _speedUpDurationInSec = 10;
+    
 
 
-    private bool isManagerActive = false;
+    
     private void Start()
-    {
-        _speedUpDurationInSec *= 1000f;
+    {       
         _triggers = GameObject.FindGameObjectsWithTag("IterationTrigger");
         for (int i = 0; i < _triggers.Length; i++) 
         {
@@ -23,41 +23,28 @@ public class SpeedUpManager : MonoBehaviour
         
     }
 
-    private void Update()
-    {
-        if (isManagerActive) 
-        {
-            while (_speedUpDurationInSec > 0)
-            {
-                _speedUpDurationInSec -= Time.deltaTime;
-                print(_speedUpDurationInSec);
-            }
 
-           
-        }
-    }
-
-    public void SpeedUp()
+    private IEnumerator SpeedUpCoroutine()
     {
         for (int i = 0; i < _itTriggers.Length; i++)
         {
             _itTriggers[i].ProductionDelay /= _speedMultiplier;
         }
 
-        
+        yield return new WaitForSeconds(10);
+
+        for (int i = 0; i < _itTriggers.Length; i++)
+        {
+            _itTriggers[i].ProductionDelay *= _speedMultiplier;
+        }
+        StopCoroutine(SpeedUpCoroutine());
     }
 
+    
+
     private void OnMouseDown()
-    {
-       if (isManagerActive == false)
-       { 
-            SpeedUp();
-            isManagerActive = true;
-       }
-       else if (isManagerActive == true) 
-       {
-            return;
-       }
+    {            
+        StartCoroutine(SpeedUpCoroutine());                      
     }
 
     
